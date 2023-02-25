6f1f6c4b014d88ebabb11da6db36af77ba2218e3
aymen723 <aymen723@hotmail.com>
2023-02-25 20:35:52 +0100

    am still working on the site

--- src/comp/page/page.js
+++ src/comp/page/page.js
@@ -1,23 +1,57 @@
 import React from "react";
 import "./pagestyle.css";
 import logo from "./ultrapics/logo.png";
-// import Sidebar from "./sidebar/sidebar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import MouseOutlinedIcon from "@mui/icons-material/MouseOutlined";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import { useRef } from "react";
 import {
-  solid,
-  regular,
-  brands,
-  icon,
-} from "@fortawesome/fontawesome-svg-core/import.macro";
+  motion,
+  useScroll,
+  useSpring,
+  // useTransform,
+  // MotionValue,
+} from "framer-motion";
+
 function Page() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  // const y = useTransform(scrollYProgress, [0, 0.5], [-300, 300]);
+  // const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+  // const y = useParallax(scrollYProgress, 300);
   return (
     <div className="page">
+      <motion.div className="progress" style={{ scaleX }} />
       <div className="landingpage">
         <div className="aboutus">
           <img src={logo} alt="logo" className="logo"></img>
           <h1 className="landigtitle">ULTRA DESING</h1>
           <h5 className="text">we are a Freelaning comapny</h5>
-          <FontAwesomeIcon icon={brands("twitter")} />
+          <div className="iconsbar">
+            <FacebookRoundedIcon
+              className="icon"
+              fontSize="medium"
+            ></FacebookRoundedIcon>
+            <InstagramIcon className="icon" fontSize="medium"></InstagramIcon>
+          </div>
+        </div>
+        <div className="scroll">
+          <h6 className="scrolltext">Scroll Down</h6>
+
+          <motion.div
+            animate={{ y: 10 }}
+            transition={{ ease: "linear", duration: 2, repeat: Infinity }}
+          >
+            <MouseOutlinedIcon
+              className="icon"
+              fontSize="large"
+            ></MouseOutlinedIcon>
+          </motion.div>
         </div>
       </div>
 
@@ -36,7 +70,9 @@ function Page() {
           <h1>a</h1>
         </div>
       </div>
-      <div className="landingpage"></div>
+      <div className="landingpage">
+        <div className="about"></div>
+      </div>
     </div>
   );
 }
